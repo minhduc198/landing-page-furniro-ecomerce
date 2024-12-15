ffaf6c3b77f695e26de9766ce066a1e192f8ce2b
nguyenminhduc1982005@gmail.com <nguyenminhduc1982005@gmail.com>
2024-12-15 20:38:34 +0700

    Feat: insporation section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,6 +8,16 @@ import { useEffect, useState } from 'react'
 import { IDataProduct } from '../../types'
 import { getListProduct } from '../../services'
 import { MAX_OUR_PRODUCTS } from '../../constants'
+import Carousel from '../../components/Carousel'
+import furniture1 from '../../assets/furniture/imgs/furniture_1.png'
+import furniture2 from '../../assets/furniture/imgs/furniture_2.png'
+import furniture3 from '../../assets/furniture/imgs/furniture_3.png'
+import furniture4 from '../../assets/furniture/imgs/furniture_4.png'
+import furniture5 from '../../assets/furniture/imgs/furniture_5.png'
+import furniture6 from '../../assets/furniture/imgs/furniture_6.png'
+import furniture7 from '../../assets/furniture/imgs/furniture_7.png'
+import furniture8 from '../../assets/furniture/imgs/furniture_8.png'
+import furniture9 from '../../assets/furniture/imgs/furniture_9.png'
 
 let limit = 0
 export default function Home() {
@@ -41,7 +51,6 @@ export default function Home() {
           <div className='btn-primary font-700'>BUY NOW</div>
         </div>
       </div>
-
       {/* CATEGORY */}
       <div className='category'>
         <div className='category-title'>Browse The Range</div>
@@ -67,7 +76,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
       {/* OUR PRODUCT */}
       <div className='our-product'>
         <div className='our-product-title'>Our Products</div>
@@ -78,6 +86,69 @@ export default function Home() {
         </div>
         <div className='btn-show-more' onClick={() => getData()}>
           {ourProducts.length < MAX_OUR_PRODUCTS ? 'Show More' : 'Show Less'}
+        </div>
+      </div>
+      {/* INSPIRATION */}
+      <div className='inspiration'>
+        <div className='inspiration-content'>
+          <div className='inspiration-title'>50+ Beautiful rooms inspiration</div>
+          <div className='inspiration-desc'>
+            Our designer already made a lot of beautiful prototipe of rooms that inspire you
+          </div>
+          <div className='btn-primary'>Explore More</div>
+        </div>
+        <div className='inspiration-carousel'>
+          <Carousel />
+        </div>
+      </div>
+      {/* Furniture */}
+      <div className='furniture'>
+        <div className='furniture-header'>
+          <div className='furniture-subtitle'>Share your setup with</div>
+          <div className='furniture-title'>#FuniroFurniture</div>
+        </div>
+        <div className='furniture-picture'>
+          <div className='picture-left'>
+            <div className='picture-top-left'>
+              <div className='pic1'>
+                <img src={furniture1} alt='' />
+              </div>
+              <div className='pic3'>
+                <img src={furniture3} alt='' />
+              </div>
+            </div>
+            <div className='picture-bottom-left'>
+              <div className='pic2'>
+                <img src={furniture2} alt='' />
+              </div>
+              <div className='pic4'>
+                <img src={furniture4} alt='' />
+              </div>
+            </div>
+          </div>
+          <div className='picture-middle'>
+            <div className='pic5'>
+              <img src={furniture5} alt='' />
+            </div>
+          </div>
+          <div className='picture-right'>
+            <div className='picture-top-right'>
+              <div className='pic6'>
+                <img src={furniture6} alt='' />
+              </div>
+              <div className='pic9'>
+                <img src={furniture9} alt='' />
+              </div>
+            </div>
+            <div className='picture-bottom-right'>
+              <div className='pic7'>
+                <img src={furniture7} alt='' />
+              </div>
+              <div className='pic8'>
+                <img src={furniture8} alt='' />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
